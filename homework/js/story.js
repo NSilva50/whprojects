83f94cac016d27d37642b8a3f42e91fb99545b30
NSilva50 <Nathansilva50@gmail.com>
2018-04-03 02:23:51 -0700

    I am happy about my progress!

---
+++ homework/js/story.js
@@ -0,0 +1,8 @@
+function startgame() {
+    var name = prompt("What is your name?");
+    console.log(name);
+    var age = prompt("How old are you?");
+    console.log(age);
+    var hobbies = prompt("What do you like to do for fun?");
+    console.log(hobbies);
+}
