a73f89fdc3d21da5eb3053d8165debd23e9760b0
NSilva50 <Nathansilva50@gmail.com>
2018-04-07 00:58:38 -0700

    I learned how to use further innerHTML in my work :)

--- homework/js/story.js
+++ homework/js/story.js
@@ -1,8 +1,16 @@
-function startgame() {
+function startgame(){
     var name = prompt("What is your name?");
     console.log(name);
     var age = prompt("How old are you?");
     console.log(age);
     var hobbies = prompt("What do you like to do for fun?");
     console.log(hobbies);
+}
+
+function lockedEntrance(){
+    document.getElementById("foyerAct1").innerHTML = "The Main Entrance is locked, there must be another way out.";
+}
+
+function lockedBasement(){
+    document.getElementById("foyerAct1").innerHTML = "The door towards the Basement is locked, there must be another way in.";
 }
