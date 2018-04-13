259466236927c4a5a08c7da8db6a954324912fea
NSilva50 <Nathansilva50@gmail.com>
2018-04-13 13:46:28 -0700

    Today opened future doors for me in java script. Thank you Matt for keeping me on task :)

---
+++ project_adventure/js/script.js
@@ -0,0 +1,7 @@
+var dA = getElementById(doorA);
+var dB = getElementById(doorB);
+var dC = getElementById(doorC);
+
+dA.onclick = function().var = ("castle");
+dB.onclick = function().var = ("swamp");
+dC.onclick = function().var = ("beach");
