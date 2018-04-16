b7a56fb59ede48607e55b3594004f0b13b2cbb49
NSilva50 <Nathansilva50@gmail.com>
2018-04-16 13:54:43 -0700

    Did it work?

--- project_adventure/js/script.js
+++ project_adventure/js/script.js
@@ -1,3 +1,21 @@
+//Character Objects
+class combatant {
+    constructor(name, hp, atk){
+        this.name = name;
+        this.hp = hp;
+        this.atk = atk;
+    }
+};
+
+var player = new combatant ("Player1", 15, 3);
+var ghosts = new combatant ("ghost", 5, 0);
+var skeletonWarriors = new combatant ("skeletonWarrior", 10, 2);
+var skeletonArchers = new combatant ("skeletonArcher", 8, 1);
+var skeletonKnights = new combatant ("skeletonKnight", 18, 3);
+
+//Combat code
+
+//Game Options
 var left = document.getElementById("left0");
 var up = document.getElementById("up0");
 var right = document.getElementById("right0");
@@ -5,50 +23,10 @@ var right = document.getElementById("right0");
 var atk = document.getElementById("atk0");
 var esc = document.getElementById("esc0");
 
-var loseGame = 0;
-console.log(loseGame);
-if (loseGame == 3) {
-    alert("You could escape no longer and have been slain. Game Over!")
-}
-
-var player = {
-    hp: "15",
-    dmg: "3",
-};
-
-var ghost = {
-    hp: "8",
-    dmg: "0",
-};
-
-var skeletonWarrior = {
-    hp: "13",
-    dmg: "1",
-};
-
-var skeletonArcher = {
-    hp: "10",
-    dmg: "x2",
-};
-
-var skeletonKnight = {
-    hp: "18",
-    dmg: "2",
-};
-
+//Story Code
 function startGame(){
+    console.log('running game');
     document.getElementById("combat").classList.add("hidden");
-}
-
-function fight(){
-    document.getElementById("movement").classList.add("hidden");
-    document.getElementById("combat").classList.remove("hidden");
-    atk.addEventListener("click", dmgCal);
-    esc.addEventListener("click", loseGame++);
-}
-
-function dmgCal(){
-    Math.floor((Math.random() * 6) + 3);
 }
 
 left.addEventListener("click", encounter1);
@@ -56,6 +34,9 @@ up.addEventListener("click", encounter1);
 right.addEventListener("click", encounter1);
 
 function encounter1(){
+    console.log('button click');
     document.getElementById("turn1").innerHTML = "You encounter a ghost. Time to see what they're made of!";
-    fight();
+    document.getElementById("movement").classList.add("hidden");
+    document.getElementById("combat").classList.remove("hidden");
+    // fight();
 }
