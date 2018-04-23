c560336082667bfb35f48b2ff33b611ec4404aea
NSilva50 <Nathansilva50@gmail.com>
2018-04-23 02:16:40 -0700

    Hit a brick wall. Need to press onward.

--- project_adventure/js/script.js
+++ project_adventure/js/script.js
@@ -1,42 +1,82 @@
-//Character Objects
+//Construction Objects
 class combatant {
-    constructor(name, hp, atk){
+    constructor(name, hp, atk /*skills*/){
         this.name = name;
         this.hp = hp;
         this.atk = atk;
+        // this.skills = skills;
     }
 };
 
+// class skills {
+//     constructor(name, atk){
+//         this.name = name;
+//         this.atk = atk;
+//     }
+// };
+
+//combatants
 var player = new combatant ("Player1", 15, 3);
 var ghosts = new combatant ("ghost", 5, 0);
 var skeletonWarriors = new combatant ("skeletonWarrior", 10, 2);
 var skeletonArchers = new combatant ("skeletonArcher", 8, 1);
 var skeletonKnights = new combatant ("skeletonKnight", 18, 3);
+var enemy = new combatant ("no one", 0, 0);
+
+//Skills
+// var fireball = new skill ("Fireball", 5);
 
 //Combat code
+function fight(){
+    document.getElementById("movement").classList.add("hidden");
+    document.getElementById("combat").classList.remove("hidden");
+    atk.addEventListener("click", basicAtk);
+    esc.addEventListener("click", escaping);
+
+    function basicAtk(){
+        var playerDamageRoll = Math.ceil(Math.random() * 5) + player.atk;
+        enemy.hp -= playerDamageRoll;
+        if(enemy.hp < 1){
+            alert("You have vanquished your foe.");
+            document.getElementById("movement").classList.remove("hidden");
+            document.getElementById("combat").classList.add("hidden");
+        }else{
+            enemyBasicAtk();
+        }
+    }
+
+    function enemyBasicAtk(){
+        var enemyDamageRoll = Math.ceil(Math.random() * 5) + enemy.atk;
+        player.hp -= enemyDamageRoll;
+        if(player.hp < 1){
+            alert("You have been defeated...");
+        }else{
+            fight();
+        }
+    }
+
+    function escaping(){
+        if(escapeCount === 3) {
+            alert("You could escape no longer and have been slain. Game Over!");
+        }else{
+            escapeCount++;
+        }
+    }
+}
 
 //Game Options
-var left = document.getElementById("left0");
-var up = document.getElementById("up0");
-var right = document.getElementById("right0");
+var move = document.getElementById("moving");
 
-var atk = document.getElementById("atk0");
-var esc = document.getElementById("esc0");
+var atk = document.getElementById("atacking");
+var esc = document.getElementById("escape");
 
-//Story Code
+var escapeCount = 0;
+console.log(escapeCount);
+
+//The Haunted Forest
 function startGame(){
     console.log('running game');
     document.getElementById("combat").classList.add("hidden");
 }
 
-left.addEventListener("click", encounter1);
-up.addEventListener("click", encounter1);
-right.addEventListener("click", encounter1);
-
-function encounter1(){
-    console.log('button click');
-    document.getElementById("turn1").innerHTML = "You encounter a ghost. Time to see what they're made of!";
-    document.getElementById("movement").classList.add("hidden");
-    document.getElementById("combat").classList.remove("hidden");
-    // fight();
-}
+move.addEventListener("click", generateEncounter);
